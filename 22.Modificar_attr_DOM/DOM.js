2311a2d9176d29fbbf0913fd1445225672be1986
Jesus Gonzalez <chuso1982@gmail.com>
2025-04-01 06:44:36 +0200

    Atributos

---
+++ 22.Modificar_attr_DOM/DOM.js
@@ -0,0 +1,16 @@
+
+const rangoEdad = document.querySelector(".rangoEdad");
+
+document.writeln(rangoEdad);
+
+// rangoEdad.setAttribute("type", "color");
+
+document.writeln(rangoEdad.getAttribute('type'));
+
+/* 
+Metodos para definir, obtener y eliminar valores de los atributos.
+
+    - setAttribute() - Modifica el valor del atributo
+    - getAttribute() - Obtiene el valor de un atributo
+    - removeAttribute() - Elimina el valor de un atributo
+*/
---
+++ 23.Atributos_Globales/DOM.js
@@ -0,0 +1,6 @@
+const titulo = document.querySelector(".titulo");
+const titulo2 = document.querySelector(".titulo2");
+
+titulo.setAttribute("contentEditable", "true");
+
+titulo2.setAttribute("hidden");
