a746fa89cc76312b95bcb75269420f1ccd6b3250
Jesus Gonzalez <chuso1982@gmail.com>
2025-04-08 06:44:05 +0200

    Arreglando cagadas

---
+++ 02.MidLevel/02.Capitulo_Dos/02.Hola/codigo.js
@@ -0,0 +1,28 @@
+// let youtubeURL = "https://youtube.com";
+
+// window.open(youtubeURL);  Abrimos una nueva ventana en el navegador
+
+// window.close(youtubeURL); // cerramos la ventana actual o la ventana en la que se llamo
+
+// window.closed; // indica si la ventana referenciada está cerrada o no.
+
+// window.stop(); // detiene la carga de recursos (deteniene la carga de la web)
+
+// window.alert("Hola, me llamo Jesús") // muestra un cuadro de alerta con el contenido especificado
+
+// prompt("Dame un dato por favor");
+
+// let respuesta = confirm("Estas seguro de salir?");
+
+// console.log(respuesta);
+
+const screen = window.screen;
+const screenLeft = window.screenLeft;
+const screenTop = window.screenTop;
+
+console.log(screen);
+document.writeln(`Screen <b>${screen}</b></br>
+    ScreenLeft <b>${screenLeft}</b><br>
+    ScreenTop <b>${screenTop}</b>`);
+
+
