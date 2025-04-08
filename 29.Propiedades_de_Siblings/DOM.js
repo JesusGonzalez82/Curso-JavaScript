8ed8fb37d8221f8d360f5022c1f401334653b914
Jesus Gonzalez <chuso1982@gmail.com>
2025-04-08 06:39:47 +0200

    Organización de carpetas

--- 29.Propiedades_de_Siblings/DOM.js
+++
@@ -1,9 +0,0 @@
-const contenedor = document.querySelector('.contenedor');
-
-const parrafo = document.createElement("p").innerHTML = 'Parrafo';
-const titulo = document.createElement('h1');
-
-const h2_antiguo = document.querySelector(".h2");
-
-console.log(h2_antiguo.nextSibling);
-console.log(h2_antiguo.nextElementSibling);
